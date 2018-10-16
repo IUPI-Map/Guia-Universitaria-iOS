f069b10cdae74a876918ff621abf09ea7113035d
estudiantes9354 <estudiantes9354@estudiantes9354s-MacBook.local>
2018-10-16 15:14:28 -0400

    Test push

---
+++ Guia Universitaria/ListaViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ListaViewController.swift
+//  Guia Universitaria
+//
+//  Created by estudiantes9354 on 10/16/18.
+//  Copyright © 2018 Archivo Universitario. All rights reserved.
+//
+
+import UIKit
+
+class ListaViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
